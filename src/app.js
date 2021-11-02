5272d840fd013a77bb5c07ef3444f2978825b7e0
bastianmoral <bastihc@gmail.com>
2021-11-02 17:14:04 -0300

    DOMAIN NAME GENERATOR BASTIAN MORAL 4 GEEKS 2021

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let Pron = ["The", "My", "Your"];
+let Adje = ["big", "best", "crazy"];
+let Noun = ["solution", "answer", "initiative", "undertaking", "disclosure"];
+let Domi = [".gov", ".tv", ".cl"];
+
+//Acá arriba dejamos todas
+const CreateNmbr = max => Math.floor(Math.random() * max);
+
+// se crea esta funcion para hacer mas pequeño el codigo de abajo
+
+const CreateTxt = () =>
+  `${Pron[CreateNmbr(Pron.length)]}${Adje[CreateNmbr(Adje.length)]}${
+    Noun[CreateNmbr(Noun.length)]
+  }${Domi[CreateNmbr(Domi.length)]}`;
+
+// con esta función se crea el nuevo dominio.
+
+const setRandomName = () => {
+  document.getElementById("domains").innerText = CreateTxt();
 };
+
+// con esta función damos la orden de renderizar el nombre.
+
+document
+  .getElementById("NameGenerator")
+  .addEventListener("click", setRandomName);
+
+// con esta funcion damos funcionalidad al boton
+
+setRandomName();
